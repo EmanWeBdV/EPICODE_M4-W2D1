de1e5ea29471e3951d5be71feb418f2c1326e7e3
Emanuele Vaccaro <evaccaro.001@gmail.com>
2026-03-26 15:05:11 +0100

    update repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 
 // TODO: Chiave API Pexels= Swx7IP1usnK1p3zVSTa9tCqpLx2hwRUgCDZdV22rdzDar3kTPyH5s1dx
 
+const apiKey = "Swx7IP1usnK1p3zVSTa9tCqpLx2hwRUgCDZdV22rdzDar3kTPyH5s1dx"
 
-const search = () => {
-
+const getPhotos = async () => {
+    try{
+        const rawData= await fetch (`https://api.pexels.com/v1/search?query=nature`, {
+            headers:{
+                "Authorization": apiKey
+            }
+        })
+        const data = await rawData.json()
+        //data.forEach(el => {
+        //   const photo = document.createElement("img")
+        //   photo.setAttribute("src", "el.photos.url")
+        //});
+        return data
+    }
+    catch(error){
+        console.log(error)
+    }
 }
+
+getPhotos()
+    .then(res => console.log(res))
